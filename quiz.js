9464d7b02aae122dd9f65922b67b22d8d1a79b99
Atelski <atelubmawuli@gmail.com>
2024-07-14 23:43:51 +0000

    Adding files for Simple Quiz Task

---
+++ quiz.js
@@ -0,0 +1,18 @@
+//function declaration to check answer
+function checkAnswer(){
+    let correctAnswer = "4";
+
+    //retrieve users answer
+    let userAnswer = document.querySelector("input[name=quiz]:checked").value;
+    
+    // getting users answer
+    if (userAnswer === correctAnswer) {
+        document.getElementById("feedback").textContent = "Correct! Well done!"
+    }
+    else {
+        document.getElementById("feedback").textContent = "That's incorrect. Try again!"
+    }
+}
+
+//button for funtion execution
+document.getElementById("submit-answer").addEventListener('click', checkAnswer);
